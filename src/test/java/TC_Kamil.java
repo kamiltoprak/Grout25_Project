d7f94eff7ed237ccbaf80148f0032d07eba4eb9b
kamiltoprak <ktoprak@gmail.com>
2020-11-01 16:22:59 -0500

    print  name on  java class

--- src/test/java/TC_Kamil.java
+++ src/test/java/TC_Kamil.java
@@ -2,5 +2,6 @@ public class TC_Kamil {
 
     public static void main(String[] args) {
         System.out.println(" KAmil toprak  is workin on project");
+        System.out.println(" KAmil toprak  is working second time  on project");
     }
 }
